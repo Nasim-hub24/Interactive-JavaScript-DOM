195cacf5a7049df7b589bbc46ab00c1e0f3f7ec5
Nasim-heb24 <ahmednasim2487@gmail.com>
2026-05-02 21:51:14 +0600

    added a commnet box using JS DOM

--- js/comment-box.js
+++ js/comment-box.js
@@ -3,7 +3,24 @@ console.log('file connect successfully')
 
 document.getElementById('post').addEventListener('click', function()
     {
-        
+        // STEP--2 : GET THE TEXT WRITTEN IN THE COMMENT TEXT AREA
+        const commentBox = document.getElementById('comment-here');
+        const newComment = commentBox.value;
+
+        // STEP--3 : GET THE PARENT NODE WHERE TO PUBLISH
+        const container = document.getElementById('commentContainer')
+
+        // CREATE A COMMENT P AND SET THE INNER TEXT
+        const commentElement = document.createElement('p')
+        commentElement.innerText = newComment;
+        commentElement.classList.add('comment');
+
+        // STEP--5 : APPEND THE NEW P TAG TO THE PARENT NODE
+        container.appendChild(commentElement);
+
+        // STEP--6 : CLEAN THE TEXT AREA
+        commentBox.value = '';
+
     }
 
 )
